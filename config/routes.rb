e44b373bebe978c0959af9f9b4f5d7010b5c0e4d
Charlie Hsieh <charlieofficial@gmail.com>
2018-01-28 12:43:32 +0800

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
     resources :articles
     devise_for :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root to: "home#index"
-    #get '/:locale' => 'articles#index'
+    root to: "articles#index"
   #end
-  get '/test' , to: 'test#index'
 end
